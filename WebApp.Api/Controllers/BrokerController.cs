705ba342fa34ec0a3f8fffad4c03a390897c5aed
Vladimir Degtyarev <vladimir.degtyarev.ext@zyfra.com>
2021-08-19 12:46:44 +0300

    initial commit

---
+++ WebApp.Api/Controllers/BrokerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.DAL.Models;
+
+namespace WebApp.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BrokerController : ControllerBase
+	{
+		[HttpPost("SendBroker")]
+		public async Task<IActionResult> SendMessage([FromBody] MessageModel message)
+		{
+			return Ok("Is working");
+		}
+	}
+}
---
+++ WebApp.DAL/Models/MessageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApp.DAL.Models
+{
+	public class MessageModel
+	{
+		public Guid MessageId { get; set; }
+		public DateTime Date { get; set; }
+		public string Message { get; set; }
+	}
+}
